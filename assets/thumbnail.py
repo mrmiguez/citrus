924a41fdc4192dbebe7d1fedef5507d9f62ce18d
Matthew Miguez <r.m.miguez@gmail.com>
2019-01-30 12:12:41 -0800

    sobek's thumbnails are a joke

--- assets/thumbnail.py
+++ assets/thumbnail.py
@@ -24,9 +24,15 @@ def thumbnail_service(identifier, tn):
                                                                   collection_list[0][8:10],
                                                                   collection_list[1])
             if doi.search(a):
-                suffix = "/{0}thm.jpg".format(a)
+                suffix_1 = "/{0}-001thm.jpg".format(a)
+                suffix_2 = "/{0}thm.jpg".format(a)
         try:
-            return prefix + sobek_tn_path + suffix
+            if requests.get(prefix + sobek_tn_path + suffix_1).status_code == 200:
+                return prefix + sobek_tn_path + suffix_1
+            elif requests.get(prefix + sobek_tn_path + suffix_2).status_code == 200:
+                return prefix + sobek_tn_path + suffix_2
+            else:
+                return None
         except UnboundLocalError:
             return None
 
