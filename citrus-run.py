1c0b9385bee070903607aa5853e8017ccdecf95f
Matthew Miguez <r.m.miguez@gmail.com>
2019-10-11 08:20:37 -0700

    update gitignore

--- citrus-run.py
+++ citrus-run.py
@@ -3,7 +3,6 @@
 import glob
 import datetime
 from os.path import abspath
-import threading
 
 # pull in config & custom transformation methods
 from assets import write_json_ld, dedupe, PATH
@@ -19,53 +18,45 @@ for key in CONFIG_DICT.keys():
 
     for xml in glob.glob(REPOX_EXPORT_DIR + '/{0}*/*.xml'.format(key)):
         if metadata == 'qdc':
-            threading.Thread(target=write_json_ld,
-                             args=(FlaLD_QDC(abspath(xml),
-                                             tn=thumbnail,
-                                             dprovide=data_provider,
-                                             iprovide=intermediate_provider), )).start()
+            write_json_ld(FlaLD_QDC(abspath(xml),
+                                    tn=thumbnail,
+                                    dprovide=data_provider,
+                                    iprovide=intermediate_provider))
         elif metadata == 'mods':
-            threading.Thread(target=write_json_ld,
-                             args=(FlaLD_MODS(abspath(xml),
-                                              tn=thumbnail,
-                                              dprovide=data_provider,
-                                              iprovide=intermediate_provider), )).start()
+            write_json_ld(FlaLD_MODS(abspath(xml),
+                                     tn=thumbnail,
+                                     dprovide=data_provider,
+                                     iprovide=intermediate_provider))
         elif metadata == 'ssdn_mods':
-            threading.Thread(target=write_json_ld,
-                             args=(SSDN_MODS(abspath(xml),
-                                             tn=thumbnail,
-                                             dprovide=data_provider,
-                                             iprovide=intermediate_provider), )).start()
+            write_json_ld(SSDN_MODS(abspath(xml),
+                                    tn=thumbnail,
+                                    dprovide=data_provider,
+                                    iprovide=intermediate_provider))
         elif metadata == 'dc':
-            threading.Thread(target=write_json_ld,
-                             args=(FlaLD_DC(abspath(xml),
-                                            tn=thumbnail,
-                                            dprovide=data_provider,
-                                            iprovide=intermediate_provider), )).start()
+            write_json_ld(FlaLD_DC(abspath(xml),
+                                   tn=thumbnail,
+                                   dprovide=data_provider,
+                                   iprovide=intermediate_provider))
         elif metadata == 'dcq':
-            threading.Thread(target=write_json_ld,
-                             args=(FlaLD_BepressDC(abspath(xml),
-                                                   tn=thumbnail,
-                                                   dprovide=data_provider,
-                                                   iprovide=intermediate_provider), )).start()
+            write_json_ld(FlaLD_BepressDC(abspath(xml),
+                                          tn=thumbnail,
+                                          dprovide=data_provider,
+                                          iprovide=intermediate_provider))
         elif metadata == 'ssdn_qdc':
-            threading.Thread(target=write_json_ld,
-                             args=(SSDN_QDC(abspath(xml),
-                                            tn=thumbnail,
-                                            dprovide=data_provider,
-                                            iprovide=intermediate_provider), )).start()
+            write_json_ld(SSDN_QDC(abspath(xml),
+                                   tn=thumbnail,
+                                   dprovide=data_provider,
+                                   iprovide=intermediate_provider))
         elif metadata == 'ssdn_dc':
-            threading.Thread(target=write_json_ld,
-                             args=(SSDN_DC(abspath(xml),
-                                           tn=thumbnail,
-                                           dprovide=data_provider,
-                                           iprovide=intermediate_provider), )).start()
+            write_json_ld(SSDN_DC(abspath(xml),
+                                  tn=thumbnail,
+                                  dprovide=data_provider,
+                                  iprovide=intermediate_provider))
         elif metadata == 'custom':
             if key == 'flmem':
-                threading.Thread(target=write_json_ld,
-                                 args=(FlMem(abspath(xml),
-                                             tn=thumbnail,
-                                             dprovide=data_provider,
-                                             iprovide=intermediate_provider), )).start()
+                write_json_ld(FlMem(abspath(xml),
+                                    tn=thumbnail,
+                                    dprovide=data_provider,
+                                    iprovide=intermediate_provider))
 
 dedupe(PATH + '/FlaLD-{0}.json'.format(datetime.date.today()))
