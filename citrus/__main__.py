1e34c01f2452b5ef8fb8510a01753985c9292cf1
Matthew Miguez <r.m.miguez@gmail.com>
2019-12-18 14:49:53 -0800

    cli formatting from here: https://python.programmingpedia.net/en/tutorial/7701/cli-subcommands-with-precise-help-output

--- citrus/__main__.py
+++ citrus/__main__.py
@@ -1,7 +1,10 @@
 import os
+import sys
+import argparse
 
 from citrus import InternetArchive, SSDNDC, dc_standard_map, SSDNQDC, qdc_standard_map, DataProvider, DPLARecord, \
     SSDNMODS, mods_standard_map, SourceResourceRequiredElementException, SourceResource
+
 
 ###############################################################################
 # Potential options for customizing transformation are:                       #
@@ -42,21 +45,21 @@ IN_PATH = 'D:\\Users\\Roland\\citrus_test'
 OUT_PATH = 'D:\\Users\\Roland\\citrus_out'
 
 ORGS = [
-        # ('fiu', SSDNDC, dc_standard_map, {'name': 'custom_field',
-        #                                   'prefix': 'http://dpanther.fiu.edu/sobek/content'},
-        #  'Florida International University Libraries', None),
-        # ('usf', SSDNDC, dc_standard_map, {'name': 'custom_field', 'prefix':
-        #     'https://digital.lib.usf.edu/content'},
-        #  'University of South Florida Libraries', None),
-        # ('um', SSDNQDC, qdc_standard_map, {'name': 'cdm', 'prefix': 'http://merrick.library.miami.edu'},
-        #  'University of Miami Libraries', None),
-        ('fsu', SSDNMODS, fsu_mods_map,
-         {'name': 'islandora', 'prefix': 'http://fsu.digital.flvc.org/islandora/object'},
-         'Florida State University Libraries', None),
-        # ('fgcu', SSDNMODS, mods_standard_map,
-        #  {'name': "islandora", 'prefix': "http://fgcu.digital.flvc.org/islandora/object"},
-        #  "Florida Gulf Coast University Library", None)
-        ]
+    # ('fiu', SSDNDC, dc_standard_map, {'name': 'custom_field',
+    #                                   'prefix': 'http://dpanther.fiu.edu/sobek/content'},
+    #  'Florida International University Libraries', None),
+    # ('usf', SSDNDC, dc_standard_map, {'name': 'custom_field', 'prefix':
+    #     'https://digital.lib.usf.edu/content'},
+    #  'University of South Florida Libraries', None),
+    # ('um', SSDNQDC, qdc_standard_map, {'name': 'cdm', 'prefix': 'http://merrick.library.miami.edu'},
+    #  'University of Miami Libraries', None),
+    ('fsu', SSDNMODS, fsu_mods_map,
+     {'name': 'islandora', 'prefix': 'http://fsu.digital.flvc.org/islandora/object'},
+     'Florida State University Libraries', None),
+    # ('fgcu', SSDNMODS, mods_standard_map,
+    #  {'name': "islandora", 'prefix': "http://fgcu.digital.flvc.org/islandora/object"},
+    #  "Florida Gulf Coast University Library", None)
+]
 
 
 # ### TESTING INTERNET ARCHIVE API SCENARIO
@@ -87,6 +90,43 @@ ORGS = [
 #         for sr in map(o.map, data):
 #             print(sr)
 
+
+class CustomHelpFormatter(argparse.HelpFormatter):
+    """
+    Custom formatting class for argparse
+    """
+
+    def _format_action(self, action):
+        if type(action) == argparse._SubParsersAction:
+            # inject new class variable for subcommand formatting
+            subactions = action._get_subactions()
+            invocations = [self._format_action_invocation(a) for a in subactions]
+            self._subcommand_max_length = max(len(i) for i in invocations)
+
+        if type(action) == argparse._SubParsersAction._ChoicesPseudoAction:
+            # format subcommand help line
+            subcommand = self._format_action_invocation(action)  # type: str
+            width = self._subcommand_max_length
+            help_text = ""
+            if action.help:
+                help_text = self._expand_help(action)
+            return "  {:{width}} -  {}\n".format(subcommand, help_text, width=width)
+
+        elif type(action) == argparse._SubParsersAction:
+            # process subcommand help section
+            msg = '\n'
+            for subaction in action._get_subactions():
+                msg += self._format_action(subaction)
+            return msg
+        else:
+            return super(CustomHelpFormatter, self)._format_action(action)
+
+
+def check():
+    print("status")
+    return 0
+
+
 def transform():
     for org in ORGS:
         o = DataProvider()
@@ -102,12 +142,16 @@ def transform():
 
 
 def main():
-    import argparse
     import configparser
-
-    parser = argparse.ArgumentParser(description='citrus - Collective Information Transformation and Reconciliation Utility Service')
-    subcommand_parsers = parser.add_subparsers(help='sub-commands')
+    parser = argparse.ArgumentParser(
+        description='citrus - Collective Information Transformation and Reconciliation Utility Service',
+        usage="[-h] [-v] [--test] | <command> [-h] | <subcommand>",
+        add_help=True,
+        formatter_class=CustomHelpFormatter)
+    subcommand_parsers = parser.add_subparsers(help='sub-commands', dest='cmd')
     subcommand_parsers.required = False
+    # subcommand_parsers.add_parser('status', help='show status')
+    # subcommand_parsers.add_parser('list', help='print list')
     harvest_parser = subcommand_parsers.add_parser('harvest', help='citrus harvest interactions')
     harvest_parser.add_argument('-r', '--run', action='store_true', help='run harvest')
     harvest_parser.add_argument('--config', action='store_true', help='view harvest config')
@@ -116,7 +160,15 @@ def main():
     transformation_parser.add_argument('--config', action='store_true', help='view transformation config')
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='verbose mode')
     parser.add_argument('--test', dest='test', action='store_true', help='run module unit tests')
-    print(parser.print_usage())
+
+    # custom help messge
+    parser._positionals.title = "commands"
+
+    # hack to show help when no arguments supplied
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+
     return parser.parse_args()
 
 
@@ -125,11 +177,11 @@ if __name__ == '__main__':
     if args.verbose:
         print('VVVvvVVvVVVVvv')  # test
 
-
     if args.test:
         ### MODULE SELF-TEST
         import unittest
         import citrus.tests
+
         loader = unittest.TestLoader()
         suite = unittest.TestSuite()
         suite.addTests(loader.loadTestsFromModule(citrus.tests))
@@ -138,4 +190,7 @@ if __name__ == '__main__':
         else:
             runner = unittest.TextTestRunner(verbosity=1)
         runner.run(suite)
-
+    if args.cmd == 'list':
+        print('list')
+    elif args.cmd == 'status':
+        sys.exit(check())
