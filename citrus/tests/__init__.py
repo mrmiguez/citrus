a40c201c41c0519c58f54a6b3be76fd2ffb238e7
Matthew Miguez <r.m.miguez@gmail.com>
2020-08-13 08:13:14 -0700

    reviewing todos (5/7)

--- citrus/tests/__init__.py
+++ citrus/tests/__init__.py
@@ -9,31 +9,30 @@ from .transform_test import *
 import configparser
 from pathlib import Path
 import sys
-#from citrus.__main__ import profile
 
 
-# Locating configs
-if os.getenv('CITRUS_CONFIG'):
-    CONFIG_PATH = Path(os.getenv('CITRUS_CONFIG'))
-elif os.path.exists(os.path.join(Path.home(), '.local/share/citrus/citrus.cfg')):
-    CONFIG_PATH = os.path.join(Path.home(), '.local/share/citrus')
-elif os.path.exists(os.path.join(Path(__file__).parents[0], 'citrus.cfg')):
-    CONFIG_PATH = Path(__file__).parents[0]
-else:
-    print("Cannot locate citrus configs.")  # TODO: This can return a more helpful prompt, or build default configs
-    CONFIG_PATH = None
+if __name__ == 'citrus.tests':
+    # Locating configs
+    if os.getenv('CITRUS_CONFIG'):
+        CONFIG_PATH = Path(os.getenv('CITRUS_CONFIG'))
+    elif os.path.exists(os.path.join(Path.home(), '.local/share/citrus/citrus.cfg')):
+        CONFIG_PATH = os.path.join(Path.home(), '.local/share/citrus')
+    elif os.path.exists(os.path.join(Path(__file__).parents[0], 'citrus.cfg')):
+        CONFIG_PATH = Path(__file__).parents[0]
+    else:
+        CONFIG_PATH = None
 
-try:
-    citrus_config = configparser.ConfigParser()
-    citrus_config.read(os.path.join(CONFIG_PATH, 'citrus.cfg'))
-    for profile in citrus_config.keys():
-        custom_map_test_path = citrus_config[profile]['CustomMapPath']
-        try:
-            sys.path.append(custom_map_test_path)
-            from custom_map_tests import *
-        except (ModuleNotFoundError, NameError):
-            pass
-except TypeError:
-    pass
+    try:
+        citrus_config = configparser.ConfigParser()
+        citrus_config.read(os.path.join(CONFIG_PATH, 'citrus.cfg'))
+        for profile in citrus_config.keys():
+            custom_map_test_path = citrus_config[profile]['CustomMapPath']
+            try:
+                sys.path.append(custom_map_test_path)
+                from custom_map_tests import *
+            except (ModuleNotFoundError, NameError):
+                pass
+    except TypeError:
+        pass
 
 
