4735798864299866f37710d45f2b144060dd9315
Matthew Miguez <r.m.miguez@gmail.com>
2021-01-22 15:35:33 -0500

    update

--- citrus_config.py
+++ citrus_config.py
@@ -15,27 +15,46 @@ import datetime
 """
 
 CONFIG_DICT = {
+    "boynton": ("dc", {'name': "custom_field" , 
+                       'prefix': "https://dpanther.fiu.edu/sobek/content"}, 
+                "Boynton Beach City Library Archives", 
+                "Florida International University Libraries"),
     'brockway': ('dc', {'name': 'custom_field',
                         'prefix': 'http://dpanther.fiu.edu/sobek/content'},
                  'Miami Shores Village Archives at Brockway Memorial Library',
                  'Florida International University Libraries'),
+    "broward": ("ssdn_mods", 
+                {'name': "islandora" , 
+                 'prefix': "https://broward.digital.flvc.org/islandora/object"}, 
+                "Broward College Archives & Special Collections", None),
     'coral_gables': ('dc', {'name': 'custom_field',
                             'prefix': 'http://dpanther.fiu.edu/sobek/content'},
                      'City of Coral Gables',
                      'Florida International University Libraries'),
+    "fau": ("ssdn_mods", 
+            {'name': "islandora" , 
+             'prefix': "https://fau.digital.flvc.org/islandora/object"}, 
+            "Florida Atlantic University", None),
     'fgcu': ('ssdn_mods', {'name': 'islandora',
-                           'prefix': 'http://fgcu.digital.flvc.org/islandora/object'},
+                      'prefix': 'http://fgcu.digital.flvc.org/islandora/object'}, 
              'Florida Gulf Coast University Library', None),
     'mbvm': ('dc', {'name': 'custom_field',
                     'prefix': 'http://dpanther.fiu.edu/sobek/content'},
              'Miami Design Preservation League, Closeup Productions',
              'Florida International University Libraries'),
+             ### mbvm must come before fiu, or FIU will be credited with these 
+             ### records instead of the Miami Design Preservation League
+             ### RIP alphabet
     'fiu': ('dc', {'name': 'custom_field',
                    'prefix': 'http://dpanther.fiu.edu/sobek/content'},
             'Florida International University Libraries', None),
     'flmem': ('custom', {'name': 'web-scrape',
                          'prefix': 'https://www.floridamemory.com'},
               'Florida Memory', None),
+    "fscj": ("ssdn_mods", 
+             {'name': "islandora" , 
+              'prefix': "https://fscj.digital.flvc.org/islandora/object"}, 
+             "Florida State College at Jacksonville", None),
     'fsu': ('mods', {'name': 'islandora',
                      'prefix': 'http://fsu.digital.flvc.org/islandora/object'},
             'Florida State University Libraries', None),                     
@@ -51,7 +70,7 @@ CONFIG_DICT = {
              'Florida International University Libraries'),
     'mdpl': ('ssdn_qdc', {'name': 'cdm', 'prefix': 'http://cdm17273.contentdm.oclc.org'},
              'Miami-Dade Public Library System', None),
-    'um': ('qdc', {'name': 'cdm', 'prefix': 'http://merrick.library.miami.edu'},
+    'um': ('qdc', {'name': 'cdm', 'prefix': 'http://cdm17191.contentdm.oclc.org'},
            'University of Miami Libraries', None),
     'usf': ('ssdn_dc', {'name': 'custom_field', 'prefix': 'https://digital.lib.usf.edu/content'},
             'University of South Florida Libraries', None),           
@@ -61,18 +80,18 @@ CONFIG_DICT = {
              'Florida International University Libraries')
 }
 
-REPOX_EXPORT_DIR = 'D:\\Users\\Roland\\citrus_test'
+REPOX_EXPORT_DIR = '/opt/ssdn/OAI_export'
 
-OUTPUT_DIR = 'D:\\Users\\Roland\\citrus_out'
+OUTPUT_DIR = '/opt/ssdn/ssdn_data'
 
-OUTPUT_FORMAT = ''
+OUTPUT_FORMAT = 'jsonl'
 
-PRETTY_PRINT = True
+PRETTY_PRINT = False
 
 PROVIDER = {'name': 'Sunshine State Digital Network',
             '@id': 'UNDETERMINED'}
 
-VERBOSE = True
+VERBOSE = False
 
 LOG_LEVEL = 'warn'
 
